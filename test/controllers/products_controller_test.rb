bc086813224642ed20e6667906534a2334ef6c39
Cmorodinka <tatyanakosachova@gmail.com>
2023-04-05 22:44:52 -0300

    Images and styles

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,3 +1,11 @@
+#---
+# Excerpted from "Agile Web Development with Rails 6",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
+#---
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
@@ -63,6 +71,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
+
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
